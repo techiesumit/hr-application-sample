52fabc4890a093dbc9289881393021b83318ef0c
Sumit Chawla <techiesumit@outlook.com>
2021-02-20 22:51:40 -0600

    Second cut added SQL and Table info

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
     Rest Exception
     Data Error
     
+# Database Schema 
+
+The Employees sample database has seven tables:
+
+    The employees table stores the data of employees.
+    The jobs table stores the job data including job title and salary range.
+    The departments table stores department data.
+    The dependents table stores the employee’s dependents.
+    The locations table stores the location of the departments of the company.
+    The countries table stores the data of countries where the company is doing business.
+    The regions table stores the data of regions such as Asia, Europe, America, and the Middle East and Africa. The countries are grouped into regions.
+#Database  ER Diagram
+![image](./readme/images/employee-schema.png)
---
+++ src/main/java/com/sumit/custom/error/example/configuration/CustomErrorApplicationConfig.java
@@ -0,0 +1,28 @@
+package com.sumit.custom.error.example.configuration;
+
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+//@Configuration
+public class CustomErrorApplicationConfig {
+
+    @Bean
+    public RestTemplate getApplicationRestTemplate() {
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory(okHttpClientRestTemplate()));
+    }
+
+    private OkHttpClient okHttpClientRestTemplate() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        ConnectionPool okHttpConnectionPool = new ConnectionPool(50, 30, TimeUnit.SECONDS);
+        builder.connectionPool(okHttpConnectionPool);
+        builder.connectTimeout(20, TimeUnit.SECONDS);
+        builder.retryOnConnectionFailure(false);
+        return builder.build();
+    }
+}
